239ec38b93b087922a2d9e7c7a156c39d4442869
busybox <planetgeek11@gmail.com>
2022-01-13 13:50:14 +0100

    views.index: Initial commit tailwind: Created configuration and build

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 4160
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'))
